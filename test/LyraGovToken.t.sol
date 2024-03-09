81d82d384e4ab285c05825bbb23568fc7c600fde
wildmolasses <changes@gmail.com>
2024-03-09 15:09:51 -0500

    mint burn upgradeable, proxy tests

--- test/LyraGovToken.t.sol
+++ test/LyraGovToken.t.sol
@@ -4,27 +4,32 @@ pragma solidity 0.8.24;
 import {Test, console} from "forge-std/Test.sol";
 import {LyraGovToken} from "../src/LyraGovToken.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract LyraGovTest is Test {
-  LyraGovToken public token;
+  LyraGovToken public tokenImpl;
+  LyraGovToken public tokenProxy;
 
   function setUp() public {
-    token = new LyraGovToken();
+    tokenImpl = new LyraGovToken();
+    tokenProxy = LyraGovToken(address(new ERC1967Proxy(address(tokenImpl), "")));
   }
 }
 
 contract Initialize is LyraGovTest {
-  function testInitialize() public {
-    assertEq(token.name(), "");
-    assertEq(token.symbol(), "");
-    token.initialize();
-    assertEq(token.name(), "Lyra Gov Token");
-    assertEq(token.symbol(), "LYRA");
+  function testInitialize(address _admin) public {
+    vm.assume(_admin != address(0));
+    assertEq(tokenProxy.name(), "");
+    assertEq(tokenProxy.symbol(), "");
+    tokenProxy.initialize(_admin);
+    assertEq(tokenProxy.name(), "Lyra Gov Token");
+    assertEq(tokenProxy.symbol(), "LYRA");
   }
 
-  function test_RevertIf_InitializeTwice() public {
-    token.initialize();
+  function test_RevertIf_InitializeTwice(address _admin) public {
+    vm.assume(_admin != address(0));
+    tokenProxy.initialize(_admin);
     vm.expectRevert(Initializable.InvalidInitialization.selector);
-    token.initialize();
+    tokenProxy.initialize(_admin);
   }
 }
