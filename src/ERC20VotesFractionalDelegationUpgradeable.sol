07e0f971adbf7081d1ef1278e6f799370238f9ba
wildmolasses <changes@gmail.com>
2024-03-09 15:23:40 -0500

    fractional delegation

---
+++ src/ERC20VotesFractionalDelegationUpgradeable.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {ERC20VotesUpgradeable} from
+  "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+
+contract ERC20VotesFractionalDelegationUpgradeable is ERC20VotesUpgradeable {}
