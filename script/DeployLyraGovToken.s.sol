046d6b2fcb37912004edec406036c5446a5afd52
wildmolasses <changes@gmail.com>
2024-04-09 11:22:00 -0400

    test deploy

--- script/DeployLyraGovToken.s.sol
+++ script/DeployLyraGovToken.s.sol
@@ -9,20 +9,22 @@ import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.s
 contract DeployLyraGovToken is Script {
   Vm.Wallet deployer;
   address admin;
+  LyraGovToken proxy;
 
-  function setUp() public {
-    uint256 deployerPrivateKey = vm.envOr("DEPLOYER_PK", uint256(12));
+  function setUp() public virtual {
+    uint256 deployerPrivateKey = vm.envOr("DEPLOYER_PRIVATE_KEY", uint256(12));
     deployer = vm.createWallet(deployerPrivateKey);
-    admin = vm.envOr("ADMIN", deployer.addr);
+    console.log("Deployer address: ", deployer.addr);
+    admin = vm.envOr("ADMIN_ADDRESS", deployer.addr);
+    console.log("Admin address: ", admin);
   }
 
-  function run() public {
+  function run() public virtual {
     vm.broadcast(deployer.privateKey);
     LyraGovToken token = new LyraGovToken();
-    console.log(msg.sender);
-    console.log(address(this));
     console.log("LyraGovToken impl deployed at: ", address(token));
-    ERC1967Proxy proxy = new ERC1967Proxy(address(token), abi.encodeWithSelector(token.initialize.selector, msg.sender));
+    proxy =
+      LyraGovToken(address(new ERC1967Proxy(address(token), abi.encodeWithSelector(token.initialize.selector, admin))));
     console.log("LyraGovToken proxy deployed at: ", address(proxy));
   }
 }
--- src/LyraGovToken.sol
+++ src/LyraGovToken.sol
@@ -2,18 +2,10 @@
 pragma solidity 0.8.24;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {AccessControlUpgradeable} from
-  "./../lib/openzeppelin-contracts-upgradeable/contracts/access/AccessControlUpgradeable.sol";
-import {AccessControlDefaultAdminRulesUpgradeable} from
-  "@openzeppelin/contracts-upgradeable/access/extensions/AccessControlDefaultAdminRulesUpgradeable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import {ERC20VotesPartialDelegationUpgradeable} from "src/ERC20VotesPartialDelegationUpgradeable.sol";
 
-contract LyraGovToken is
-  UUPSUpgradeable,
-  AccessControlUpgradeable,
-  // AccessControlDefaultAdminRulesUpgradeable,
-  ERC20VotesPartialDelegationUpgradeable
-{
+contract LyraGovToken is UUPSUpgradeable, AccessControlUpgradeable, ERC20VotesPartialDelegationUpgradeable {
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
   bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
@@ -23,7 +15,7 @@ contract LyraGovToken is
   }
 
   function initialize(address _admin) public initializer {
-    __ERC20_init("Lyra Gov Token", "LYRA");
+    __ERC20_init("Lyra Governance Token", "gLyra");
     __AccessControl_init();
     if (_admin == address(0)) {
       revert("Admin cannot be the zero address");
