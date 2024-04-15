373fafe2e6c9ed8d4c059e3db64a72947928c465
Kent Fenwick <kent.fenwick@gmail.com>
2024-04-15 13:41:05 -0400

    updated Name to L2GovToken

---
+++ script/DeployL2GovToken.s.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {L2GovToken} from "src/L2GovToken.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+contract DeployL2GovToken is Script {
+    Vm.Wallet deployer;
+    address admin;
+    L2GovToken proxy;
+
+    function setUp() public virtual {
+        uint256 deployerPrivateKey = vm.envOr(
+            "DEPLOYER_PRIVATE_KEY",
+            uint256(12)
+        );
+        deployer = vm.createWallet(deployerPrivateKey);
+        console.log("Deployer address: ", deployer.addr);
+        admin = vm.envOr("ADMIN_ADDRESS", deployer.addr);
+        console.log("Admin address: ", admin);
+    }
+
+    function run() public virtual {
+        vm.broadcast(deployer.privateKey);
+        L2GovToken token = new L2GovToken();
+        console.log("L2GovToken impl deployed at: ", address(token));
+        proxy = L2GovToken(
+            address(
+                new ERC1967Proxy(
+                    address(token),
+                    abi.encodeWithSelector(token.initialize.selector, admin)
+                )
+            )
+        );
+        console.log("L2GovToken proxy deployed at: ", address(proxy));
+    }
+}
--- test/L2GovToken.t.sol
+++ test/L2GovToken.t.sol
@@ -2,21 +2,21 @@
 pragma solidity 0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
-import {LyraGovToken} from "../src/LyraGovToken.sol";
+import {L2GovToken} from "../src/L2GovToken.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-contract LyraGovTest is Test {
-  LyraGovToken public tokenImpl;
-  LyraGovToken public tokenProxy;
+contract L2GovTest is Test {
+  L2GovToken public tokenImpl;
+  L2GovToken public tokenProxy;
 
   function setUp() public {
-    tokenImpl = new LyraGovToken();
-    tokenProxy = LyraGovToken(address(new ERC1967Proxy(address(tokenImpl), "")));
+    tokenImpl = new L2GovToken();
+    tokenProxy = L2GovToken(address(new ERC1967Proxy(address(tokenImpl), "")));
   }
 }
 
-contract Initialize is LyraGovTest {
+contract Initialize is L2GovTest {
   function testInitialize(address _admin) public {
     vm.assume(_admin != address(0));
     assertEq(tokenProxy.name(), "");
