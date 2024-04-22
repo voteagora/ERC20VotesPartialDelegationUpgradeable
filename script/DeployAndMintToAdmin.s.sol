cb21b759fc6079a85405c151f1ec8a9f062475cf
wildmolasses <changes@gmail.com>
2024-04-22 08:05:53 -0400

    L2GovToken

--- script/DeployAndMintToAdmin.s.sol
+++ script/DeployAndMintToAdmin.s.sol
@@ -5,17 +5,17 @@ import {Script, console} from "forge-std/Script.sol";
 import {DeployL2GovToken} from "./DeployL2GovToken.s.sol";
 
 contract DeployAndMintToAdmin is DeployL2GovToken {
-    function setUp() public virtual override {
-        super.setUp();
-        admin = deployer.addr;
-    }
+  function setUp() public virtual override {
+    super.setUp();
+    admin = deployer.addr;
+  }
 
-    function run() public virtual override {
-        super.run();
-        vm.startBroadcast(deployer.privateKey);
-        proxy.grantRole(proxy.MINTER_ROLE(), admin);
-        proxy.mint(admin, type(uint208).max);
-        vm.stopBroadcast();
-        console.log("gLYRA admin balance: ", proxy.balanceOf(admin));
-    }
+  function run() public virtual override {
+    super.run();
+    vm.startBroadcast(deployer.privateKey);
+    proxy.grantRole(proxy.MINTER_ROLE(), admin);
+    proxy.mint(admin, type(uint208).max);
+    vm.stopBroadcast();
+    console.log("L2GovToken admin balance:\t", proxy.balanceOf(admin));
+  }
 }
