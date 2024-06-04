417c286a1598acc1044f93a536d20fc961d0dd65
Ed Mazurek <changes@gmail.com>
2024-06-04 13:14:49 -0400

    mint and burn tests (#42)

--- src/L2GovToken.sol
+++ src/L2GovToken.sol
@@ -5,44 +5,33 @@ import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/U
 import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import {ERC20VotesPartialDelegationUpgradeable} from "src/ERC20VotesPartialDelegationUpgradeable.sol";
 
-contract L2GovToken is
-    UUPSUpgradeable,
-    AccessControlUpgradeable,
-    ERC20VotesPartialDelegationUpgradeable
-{
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+contract L2GovToken is UUPSUpgradeable, AccessControlUpgradeable, ERC20VotesPartialDelegationUpgradeable {
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+  bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
+  /// @custom:oz-upgrades-unsafe-allow constructor
+  constructor() {
+    _disableInitializers();
+  }
+
+  function initialize(address _admin) public initializer {
+    __ERC20_init("L2 Governance Token", "gL2");
+    __EIP712_init("L2 Governance Token", "1");
+    __ERC20Permit_init("L2 Governance Token");
+    __AccessControl_init();
+    if (_admin == address(0)) {
+      revert InvalidAddressZero();
     }
+    _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+  }
 
-    function initialize(address _admin) public initializer {
-        __ERC20_init("L2 Governance Token", "gL2");
-        __EIP712_init("L2 Governance Token", "1");
-        __AccessControl_init();
-        if (_admin == address(0)) {
-            revert InvalidAddressZero();
-        }
-        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
-    }
+  function _authorizeUpgrade(address) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
 
-    function _authorizeUpgrade(
-        address
-    ) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
+  function mint(address _account, uint256 _amount) public onlyRole(MINTER_ROLE) {
+    _mint(_account, _amount);
+  }
 
-    function mint(
-        address _account,
-        uint256 _amount
-    ) public onlyRole(MINTER_ROLE) {
-        _mint(_account, _amount);
-    }
-
-    function burn(
-        address _account,
-        uint256 _value
-    ) public onlyRole(BURNER_ROLE) {
-        _burn(_account, _value);
-    }
+  function burn(address _account, uint256 _value) public onlyRole(BURNER_ROLE) {
+    _burn(_account, _value);
+  }
 }
