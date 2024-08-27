bf9e21a4e9292546415b556125324b7d5f2c73ba
Kent Fenwick <kent.fenwick@gmail.com>
2024-08-27 13:26:56 -0400

    Updated interface

--- src/IVotesPartialDelegation.sol
+++ src/IVotesPartialDelegation.sol
@@ -4,13 +4,13 @@ pragma solidity 0.8.26;
 import {IERC6372} from "@openzeppelin/contracts/interfaces/IERC6372.sol";
 
 struct PartialDelegation {
-  address _delegatee;
-  uint96 _numerator;
+    address _delegatee;
+    uint96 _numerator;
 }
 
 struct DelegationAdjustment {
-  address _delegatee;
-  uint208 _amount;
+    address _delegatee;
+    uint208 _amount;
 }
 
 /**
@@ -18,57 +18,92 @@ struct DelegationAdjustment {
  * @custom:security-contact security@voteagora.com
  */
 interface IVotesPartialDelegation is IERC6372 {
-  /**
-   * @dev The signature used has expired.
-   */
-  error VotesExpiredSignature(uint256 expiry);
+    /**
+     * @dev The signature used has expired.
+     */
+    error VotesExpiredSignature(uint256 expiry);
 
-  /**
-   * @dev Emitted when an account changes their delegate.
-   */
-  event DelegateChanged(
-    address indexed delegator, PartialDelegation[] oldDelegatees, PartialDelegation[] newDelegatees
-  );
+    /**
+     * @dev Emitted when an account changes their delegate.
+     */
+    event DelegateChanged(
+        address indexed delegator,
+        PartialDelegation[] oldDelegatees,
+        PartialDelegation[] newDelegatees
+    );
 
-  /**
-   * @dev Emitted when a token transfer or delegate change results in changes to a delegate's number of voting units.
-   */
-  event DelegateVotesChanged(address indexed delegate, uint256 previousVotes, uint256 newVotes);
+    /**
+     * @dev Emitted when a token transfer or delegate change results in changes to a delegate's number of voting units.
+     */
+    event DelegateVotesChanged(
+        address indexed delegate,
+        uint256 previousVotes,
+        uint256 newVotes
+    );
 
-  /**
-   * @dev Returns the current amount of votes that `account` has.
-   */
-  function getVotes(address account) external view returns (uint256);
+    /**
+     * @dev Returns the current amount of votes that `account` has.
+     */
+    function getVotes(address account) external view returns (uint256);
 
-  /**
-   * @dev Returns the amount of votes that `account` had at a specific moment in the past. If the `clock()` is
-   * configured to use block numbers, this will return the value at the end of the corresponding block.
-   */
-  function getPastVotes(address account, uint256 timepoint) external view returns (uint256);
+    /**
+     * @dev Returns the amount of votes that `account` had at a specific moment in the past. If the `clock()` is
+     * configured to use block numbers, this will return the value at the end of the corresponding block.
+     */
+    function getPastVotes(
+        address account,
+        uint256 timepoint
+    ) external view returns (uint256);
 
-  /**
-   * @dev Returns the total supply of votes available at a specific moment in the past. If the `clock()` is
-   * configured to use block numbers, this will return the value at the end of the corresponding block.
-   *
-   * NOTE: This value is the sum of all available votes, which is not necessarily the sum of all delegated votes.
-   * Votes that have not been delegated are still part of total supply, even though they would not participate in a
-   * vote.
-   */
-  function getPastTotalSupply(uint256 timepoint) external view returns (uint256);
+    /**
+     * @dev Returns the total supply of votes available at a specific moment in the past. If the `clock()` is
+     * configured to use block numbers, this will return the value at the end of the corresponding block.
+     *
+     * NOTE: This value is the sum of all available votes, which is not necessarily the sum of all delegated votes.
+     * Votes that have not been delegated are still part of total supply, even though they would not participate in a
+     * vote.
+     */
+    function getPastTotalSupply(
+        uint256 timepoint
+    ) external view returns (uint256);
 
-  /**
-   * @dev Returns the delegate that `account` has chosen.
-   * Removed: This function is incompatible with partial delegation, which allows for multiple delegates per account.
-   */
-  //   function delegates(address account) external view returns (address);
+    /**
+     * @dev Return the latest voteable supply
+     */
+    function getVoteableSupply() external view returns (uint256);
 
-  /**
-   * @dev Delegates votes from the sender to `delegatee`.
-   */
-  function delegate(address delegatee) external;
+    /**
+     * @dev Return the voteable supply at a gievn block number
+     */
+    function getPastVoteableSupply(
+        uint256 timepoint
+    ) external view returns (uint256);
 
-  /**
-   * @dev Delegates votes from signer to `delegatee`.
-   */
-  function delegateBySig(address delegatee, uint256 nonce, uint256 expiry, uint8 v, bytes32 r, bytes32 s) external;
+    /**
+     * @dev Return the number of active delegates
+     */
+    function getActiveDelgateesCount() external view returns (uint256);
+
+    /**
+     * @dev Returns the delegate that `account` has chosen.
+     * Removed: This function is incompatible with partial delegation, which allows for multiple delegates per account.
+     */
+    //   function delegates(address account) external view returns (address);
+
+    /**
+     * @dev Delegates votes from the sender to `delegatee`.
+     */
+    function delegate(address delegatee) external;
+
+    /**
+     * @dev Delegates votes from signer to `delegatee`.
+     */
+    function delegateBySig(
+        address delegatee,
+        uint256 nonce,
+        uint256 expiry,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
 }
